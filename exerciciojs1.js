f634e3aad60f96a59ef76f30e7e7290301c5d14c
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-01 18:56:35 -0300

    exercise 5

--- exerciciojs1.js
+++ exerciciojs1.js
@@ -1,3 +1,4 @@
+/*
 let a = 10;
 let b = 15;
 console.log(a + b);
@@ -5,3 +6,13 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+*/
+
+let anguloA = 80;
+let anguloB = 25;
+let anguloC = 75;
+if (anguloA + anguloB + anguloC == 180) {
+    console.log('true');
+} else {
+    console.log('false');
+} 
