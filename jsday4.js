87825cea8a606be4725c4dab6dbdd370ae2d7174
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-06 13:41:31 -0300

    Exercices for in and for of

--- jsday4.js
+++ jsday4.js
@@ -14,3 +14,23 @@ console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + play
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]; 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor jogadora do mundo por ' + player['bestInTheWorld'].length + ' vezes')
 console.log('A jogadora possui ' + player.medals['golden'] + ' medalhas de ouro e ' + player.medals['silver'] + ' medalhas de prata');
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+}
+
+for(let value in names) {
+  console.log('Olá, ' + names[value]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for(let value2 in car) {
+  console.log(value2, car[value2]);
+}
