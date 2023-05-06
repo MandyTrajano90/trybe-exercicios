d902ecb2eb041d45efe25188e603782ee6be7ecf
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-06 16:14:36 -0300

    Object keys

--- jsday4.js
+++ jsday4.js
@@ -93,3 +93,33 @@ let cidade = 'Niteroi';
 adicionaPropriedade(student, novaChave, cidade);
 console.log(student);
 console.log(student.primeiroNome + ' ' + student.ultimoNome + ' é estudante na Trybe, tem ' + student['idade'] + ' anos. Ela mora no estado do ' + student.estado + ', na cidade de ' + student.cidade + '.' + ' Ela tem namorada? ' + student.temNamorada + '.');
+
+// Object.keys //
+
+let student1 = {
+  html: 'Muito Bom',
+  css: 'Bom',
+  javascript: 'Ótimo',
+  softskills: 'Ótimo',
+};
+
+let student2 = {
+  html: 'Bom',
+  css: 'Ótimo',
+  javascript: 'Ruim',
+  softskills: 'Ótimo',
+  git: 'Bom', // chave adicionada
+};
+
+function listSkills(student) {
+  let arrayOfSkills = Object.keys(student);
+  for (let index in arrayOfSkills) {
+    console.log(arrayOfSkills[index] + ', Nível: ' + student[arrayOfSkills[index]]);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
