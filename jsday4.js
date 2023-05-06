b2bd935d6d38c9ee429a294a30242df958d8001d
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-06 20:29:00 -0300

    First - Margarida e Tio Patinhas

--- jsday4.js
+++ jsday4.js
@@ -177,3 +177,60 @@ for(index in pairKeyValue) {
   console.log('País:', pairKeyValue[index][0]);
   console.log('Capital:', pairKeyValue[index][1]);
 };
+
+// Object assign //
+let person = {
+  name:'Roberto',
+};
+
+let lastName2 = {
+  lastName: 'Silva',
+};
+
+let newPerson = Object.assign({},person,lastName2);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
+
+// Exercise - objects and for/in //
+/*
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+console.log('Bem-vinda, ' + info.personagem);
+info.recorrente = 'Sim';
+console.log(info);
+
+for(let chave in info) {
+  console.log(chave);
+}
+
+for(let value in info) {
+  console.log(info[value]);
+}
+*/
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+}
+
+for(let key in info) {
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+};
