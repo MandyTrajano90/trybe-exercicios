80941751b07c7a374124adb6fd54058575ec2ba3
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-06 21:34:03 -0300

    Exercise - Leitura de objetos

--- jsday4.js
+++ jsday4.js
@@ -234,3 +234,31 @@ for(let key in info) {
     console.log(info[key] + ' e ' + info2[key]);
   }
 };
+
+// Leitura de objetos //
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+leitor.livrosFavoritos.outroLivro = {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+console.log('Julia tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
