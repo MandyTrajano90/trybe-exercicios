35f5f18bd52f070d3f2139b4d7477f0a16ec8f21
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-15 10:36:11 -0300

    Studying DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+const whereAreYou = document.getElementById('where-are-you');
+const parent = whereAreYou.parentElement;
+parent.style.color = 'blue';
+const firstChild = document.getElementById('first-child-of-child');
+firstChild.innerText = 'Sweet child';
+const firstC = parent.firstElementChild;
+const primeiroFilho = whereAreYou.previousElementSibling;
+const textElement = whereAreYou.nextSibling;
+const thirdChild = whereAreYou.nextElementSibling;
+const thirdChild2 = parent.lastElementChild.previousElementSibling;
