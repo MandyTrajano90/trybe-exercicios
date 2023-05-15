3866b5da24736983487ac8525cee8b4d8318170c
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-15 11:27:51 -0300

    Other exercise

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const whereAreYou = document.getElementById('where-are-you');
 const parent = whereAreYou.parentElement;
 parent.style.color = 'blue';
@@ -7,4 +8,25 @@ const firstC = parent.firstElementChild;
 const primeiroFilho = whereAreYou.previousElementSibling;
 const textElement = whereAreYou.nextSibling;
 const thirdChild = whereAreYou.nextElementSibling;
-const thirdChild2 = parent.lastElementChild.previousElementSibling;
+const thirdChild2 = parent.lastElementChild.previousElementSibling; */ 
+
+const pai = document.getElementById('parent');
+const irmao = document.createElement('section');
+irmao.id = 'irmao de elemento';
+pai.appendChild(irmao);
+
+const elementoOndeVoceEsta = document.getElementById('where-are-you');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'Filho elemento onde voce esta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilho = document.getElementById('first-child-of-child');
+const filhoDoPrimeiroFilho = document.createElement('section');
+filhoDoPrimeiroFilho.id = 'eu sou filho do primeiro filho';
+primeiroFilho.appendChild(filhoDoPrimeiroFilho);
+
+const terceiroFilho = filhoDoPrimeiroFilho
+.parentElement
+.parentElement
+.nextElementSibling;
+console.log(terceiroFilho);
