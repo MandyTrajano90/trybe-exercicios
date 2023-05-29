483dee0cd8dc565bca647a62eb85dd0c353e830a
Mandytrajano90 <mandytrajano90@gmail.com>
2023-05-29 10:54:58 -0300

    Finishing Trybe Sporting Goods

--- script.js
+++ script.js
@@ -21,17 +21,65 @@ button.addEventListener('click', () => {
   showPromo(name, number);
 });
 
+/*
 const showPromo = (name, number) => {
   const firstText = document.querySelector('#text-initial');
   const secondText = document.querySelector('#text-final');
-  const productObject = checkNumber(parseInt(number));
+  const productObject = checkNumber(parseInt(number));  
 
   firstText.innerHTML = `Boas-vindas, ${name}!`;
   secondText.innerHTML = `A promoção do dia é: 
     ${productObject.product} no valor de R$ ${productObject.price}`;
+} */
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    checkValidRange(number);
+    const productObject = checkPromo(number);
+
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: 
+      ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(err) {
+    secondText.innerHTML = err.message;
+  }
+}
+/*
+// Segunda parte
+const checkNumber = (number) => {
+  return promo[number - 1];
+} */
+
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
 }
 
 // Segunda parte
 const checkNumber = (number) => {
-  return promo[number - 1];
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+}
+
+// Terceira parte
+const checkPromo = (number) => {
+  for (let index = 0; index < promo.length; index += 1) {
+    if (number - 1 === index) {
+      return promo[index];
+    }
+  }
+}
+
+const checkValidRange = (number) => {
+  if (number < 1 || number > 10) {
+    throw new Error ('É necessário digitar um número entre 1 e 10.');
+  }
 }
